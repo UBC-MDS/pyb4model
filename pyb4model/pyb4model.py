254434a920899dc4ed078c1196c0bd2eae939046
jake1125 <agdal1125@gmail.com>
2020-03-03 15:11:17 -0800

    updated feature_selection function & testing function

--- pyb4model/pyb4model.py
+++ pyb4model/pyb4model.py
@@ -1,3 +1,8 @@
+import numpy as  np
+import pandas as pd
+import pytest
+from sklearn.model_selection import cross_val_score
+
 def missing_val(df, method):
     """
     Handles missing values.
@@ -61,49 +66,147 @@ def fit_and_report(model, X, y, Xv, yv, m_type = 'regression'):
     return errors
 
 
-import numpy as  np
-from sklearn.model_selection import cross_val_score
 
-class ForSelect:
-    def __init__(self, model,
-                 min_features=None,
-                 max_features=None,
-                 scoring=None,
-                 cv=None):
+def ForSelect(model,
+            data_feature,
+            data_label,
+            min_features=1,
+            max_features=None,
+            problem_type='regression'
+            cv=3):
         """
-        Defining Class
+
+        Implementation of forward selection algorithm.
+        Search and score with mean cross validation score using feature candidates and
+        add features with the best score each step.
+        Uses mean squared error for regression, accuracy for classification problem.
+
         @params
         --------
         model: object            -- sklearn model object
+        data_feature: object     -- pandas DataFrame object (features/predictors/explanatory variables)
+        data_label: object       -- pandas Series object (labels)
         min_features: integer    -- number of mininum features to select
         max_features: integer    -- number of maximum features to select
-        scoring:  string         -- sklearn scoring metric
+        problem_type: string     -- problem type {"classification", "regression"}
         cv: integer              -- k for k-fold-cross-validation
-        @example
-        --------
-        fs = ForSelect
-        """
 
-    def fit(self, X, y):
-        """
-        - Implementation of forward selection algorithm.
-        - Search and score with mean cross validation score using feature candidates and
-          add features with the best score each step.
-        - Return dataset with selected features.
-        @params
-        --------
-        X: array       -- training dataset (features)
-        y: array       -- training dataset (labels)
         @returns
         --------
-        self          -- with updated self.ftr_
+        list                     -- a list of selected column/feature names 
+
+
         @example
-        -------
+        --------
         rf = RandomForestClassifier()
-        fs = ForSelect(rf, min_features=2, max_features=5)
-        fs.fit(X_train, y_train)
-        fs.ftr_
+        selected_features = ForSelect(rf, 
+                                    X_train, 
+                                    y_train,
+                                    min_features=2, 
+                                    max_features=5, 
+                                    scoring="neg_mean_square",
+                                    problem_type="regression", 
+                                    cv=4)
+        new_X_train = X_train[selected_features]
         """
+
+        # Test the input & arguments
+        Test_ForSelect()
+
+        # Create Empty Feature list
+        ftr_ = []
+
+        # Define maximum amount of features
+        if max_features is None:
+            max_features = X.shape[1]
+
+        # total list of features
+        total_ftr = list(range(0, X.shape[1]))
+
+        # define scoring
+        if problem_type=="regression":
+            scoring='neg_mean_squared_error',
+        else:
+            scoring='accuracy'
+        
+        # initialize error score
+        best_score = -np.inf
+
+        i = 0
+
+        while len(ftr_) < max_features:
+            # remove already selected features
+            features_unselected = list(set(total_ftr) - set(ftr_))
+
+            # Initialize potential candidate feature to select
+            candidate = None
+
+            # Iterate
+            for feature in features_unselected:
+                ftr_candidate = ftr_ + [feature]
+                eval_score = np.mean(cross_val_score(model,
+                                                     X[:, ftr_candidate],
+                                                     y,
+                                                     cv=cv,
+                                                     scoring=scoring))
+
+                # If computed error score is better than our current best score
+                if eval_score > best_score:
+                    best_score = eval_score  # Overwrite the best_score
+                    candidate = feature  # Consider the feature as candidate
+
+            # Add the selected feature
+            if candidate is not None:
+                ftr_.append(candidate)
+
+                # Report Progress
+                i = i + 1
+                if i % 5 == 0:
+                    print("{} Iterations Done".format(i))
+                    print("current best score: {}".format(best_score))
+
+                    print("Current selected features: {}".format(ftr_))
+                    print("\n")
+
+            else:
+                # End process
+                print("{} iterations in total".format(i))
+                print("Final selected features: {}".format(ftr_))
+                break
+
+        # End Process
+        print("{} iterations in total".format(i))
+        print("Final selected features: {}".format(ftr_))
+
+        return ftr_
+
+
+def Test_ForSelect():
+
+    # Test Input Types
+    assert "sklearn" in str(type(model))
+    assert type(max_features) == int
+    assert type(min_features) == int
+    assert type(cv) == int
+    if isinstance(data_feature, pd.DataFrame):
+        pass
+    else:
+        raise TypeError("Your data_feature must be a pd.DataFrame object")
+    if isinstance(data_label, pd.DataFrame):
+        pass
+    else:
+        raise TypeError("Your data_label must be a pd.Series object")
+    assert 
+    assert problem_type is in ["classification", "regression"]
+    assert data_feature.shape[0] == data_label.shape[0]
+    
+    print("Input Type Test passed")
+
+    return 
+
+    
+
+
 
         
 import pandas as pd
