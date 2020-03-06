cb48b624c38efad1a4d98f72d0519357eb741a35
jake1125 <agdal1125@gmail.com>
2020-03-06 13:03:30 -0800

    moved test function to test_pyb4model.py

--- pyb4model/pyb4model.py
+++ pyb4model/pyb4model.py
@@ -180,29 +180,7 @@ def ForSelect(model,
 
         return ftr_
 
-
-def Test_ForSelect():
-
-    # Test Input Types
-    assert "sklearn" in str(type(model))
-    assert type(max_features) == int
-    assert type(min_features) == int
-    assert type(cv) == int
-    if not isinstance(data_feature, pd.DataFrame):
-        raise TypeError("Your data_feature must be a pd.DataFrame object")
-    if not isinstance(data_label, pd.DataFrame):
-        raise TypeError("Your data_label must be a pd.Series object")
-    assert 
-    assert problem_type is in ["classification", "regression"]
-    assert data_feature.shape[0] == data_label.shape[0]
     
-    print("Input Type Test passed")
-
-    return 
-
-    
-
-
 
         
 import pandas as pd
--- tests/test_pyb4model.py
+++ tests/test_pyb4model.py
@@ -1,1 +1,21 @@
 from pyb4model import pyb4model
+
+
+def Test_ForSelect():
+
+    # Test Input Types
+    assert "sklearn" in str(type(model))
+    assert type(max_features) == int
+    assert type(min_features) == int
+    assert type(cv) == int
+    if not isinstance(data_feature, pd.DataFrame):
+        raise TypeError("Your data_feature must be a pd.DataFrame object")
+    if not isinstance(data_label, pd.DataFrame):
+        raise TypeError("Your data_label must be a pd.Series object")
+    assert 
+    assert problem_type is in ["classification", "regression"]
+    assert data_feature.shape[0] == data_label.shape[0]
+    
+    print("Input Type Test passed")
+
+    return 
