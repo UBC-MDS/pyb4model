825f80b8f8138795f9cda01dc92d6448d44b07e8
Andrea Lee <45249215+andrealee011@users.noreply.github.com>
2020-03-18 13:48:27 -0700

    remove unused packages

--- pyb4model/pyb4model.py
+++ pyb4model/pyb4model.py
@@ -3,9 +3,6 @@ from sklearn.impute import KNNImputer
 from sklearn.model_selection import cross_val_score
 import numpy as np
 import pandas as pd
-from sklearn import datasets
-from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
 
 
 def missing_val(df, method):
@@ -103,6 +100,8 @@ def fit_and_report(model, X, y, Xv, yv, m_type='regression'):
 
     Examples
     --------
+    >>> from sklearn import datasets
+    >>> from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
     >>> iris = datasets.load_iris(return_X_y = True)
     >>> knn_c = KNeighborsClassifier()
     >>> knn_r = KNeighborsRegressor()
@@ -175,6 +174,8 @@ def ForSelect(
 
     Example
     --------
+    >>> from sklearn import datasets
+    >>> from sklearn.ensemble import RandomForestClassifier
     >>> rf = RandomForestClassifier()
     >>> iris = datasets.load_iris(return_X_y = True)
     >>> X_train = pd.DataFrame(iris[0][1:100])
